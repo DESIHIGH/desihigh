48042c0ff5f53f0fb390fe859e96149fcdf7868a
Michael James Wilson <MJWilson@mjwilson-m65.attlocal.net>
2020-07-08 15:25:00 -0700

    Update.

--- xkcd.py
+++ xkcd.py
@@ -1,26 +1,34 @@
+import pylab      as     pl
+import pandas     as     pd
+import numpy      as     np
+
 from   matplotlib import pyplot as plt
-import numpy as np
 
-plt.xkcd()
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-plt.xticks([])
-plt.yticks([])
-ax.set_ylim([-30, 10])
+def xkcd_plot():
+  plt.xkcd()
 
-data = np.ones(100)
-data[70:] -= np.arange(30)
+  fig = plt.figure()
+  ax  = fig.add_subplot(1, 1, 1)
+  ax.spines['right'].set_color('none')
+  ax.spines['top'].set_color('none')
 
-plt.annotate(
-    'THE DAY I REALIZED\nI COULD COOK BACON\nWHENEVER I WANTED',
-    xy=(70, 1), arrowprops=dict(arrowstyle='->'), xytext=(15, -10))
+  # plt.xticks([])
+  # plt.yticks([])
 
-plt.plot(data)
+  return fig, ax
 
-plt.xlabel('time')
-plt.ylabel('my overall health')
 
-plt.show()
+if __name__ == '__main__':
+  fig, ax = xkcd_plot()
+
+  dat = pd.read_csv('hubble.dat', sep='\s+')
+
+  ax.plot(dat['r'], dat['v'])
+  
+  ax.set_xlabel('Distance from us [Mpc]')
+  ax.set_ylabel('Recession velocity [km/s]')
+
+  plt.tight_layout()
+  
+  plt.show()

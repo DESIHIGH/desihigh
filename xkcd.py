0d0802f4bee080af8a25d61d4941b96b1a30c847
Michael James Wilson <MJWilson@mjwilson-m65.attlocal.net>
2020-07-08 16:11:22 -0700

    Update.

--- xkcd.py
+++ xkcd.py
@@ -3,32 +3,18 @@ import pandas     as     pd
 import numpy      as     np
 
 from   matplotlib import pyplot as plt
+from   scipy      import stats
 
 
-def xkcd_plot():
+def plot_xkcd():
   plt.xkcd()
 
   fig = plt.figure()
   ax  = fig.add_subplot(1, 1, 1)
+
   ax.spines['right'].set_color('none')
   ax.spines['top'].set_color('none')
 
-  # plt.xticks([])
-  # plt.yticks([])
-
-  return fig, ax
+  return  fig, ax
 
 
-if __name__ == '__main__':
-  fig, ax = xkcd_plot()
-
-  dat = pd.read_csv('hubble.dat', sep='\s+')
-
-  ax.plot(dat['r'], dat['v'])
-  
-  ax.set_xlabel('Distance from us [Mpc]')
-  ax.set_ylabel('Recession velocity [km/s]')
-
-  plt.tight_layout()
-  
-  plt.show()

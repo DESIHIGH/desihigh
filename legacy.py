d9ae4e4a3152d2236f80843cfc28b57405bc6f21
Michael James Wilson <MJWilson@mjwilson-m65.attlocal.net>
2020-07-13 13:32:21 -0700

    Update.

--- legacy.py
+++ legacy.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot       as     plt
 def cutout(ax, ra, dec):  
   url       = 'http://legacysurvey.org/viewer/jpeg-cutout?ra={:.4f}&dec={:.4f}&layer=decals-dr7&pixscale=0.27&bands=grz'.format(ra, dec)
 
-  image     = plt.imread("http://matplotlib.sourceforge.net/_static/logo2.png")
+  image     = plt.imread(url)
   
   ax.imshow(image)
   

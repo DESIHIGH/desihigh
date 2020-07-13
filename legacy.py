08e1dfeb6bdae7a7245c931cffc2dca510c17101
Michael James Wilson <MJWilson@mjwilson-m65.attlocal.net>
2020-07-13 13:49:13 -0700

    Update.

--- legacy.py
+++ legacy.py
@@ -1,13 +1,14 @@
 import requests
 import matplotlib.pyplot       as     plt
 
-from PIL import Image
-
+from   PIL                     import  Image
+from   io                      import  BytesIO
 
 def cutout(ax, ra, dec):  
   url       = 'http://legacysurvey.org/viewer/jpeg-cutout?ra={:.4f}&dec={:.4f}&layer=decals-dr7&pixscale=0.27&bands=grz'.format(ra, dec)
 
-  image     = Image.open(url)
+  response  = requests.get(url)
+  img       = Image.open(BytesIO(response.content))
   
-  ax.imshow(image)
+  ax.imshow(img)
   

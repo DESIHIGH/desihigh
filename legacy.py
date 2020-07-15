7444264128b740650db38cef1e24b7bfb51bf34d
Michael James Wilson <MJWilson@mjwilson-m65.attlocal.net>
2020-07-15 13:00:11 -0700

    Updated legacy cutout from DR7 to DR8.

--- legacy.py
+++ legacy.py
@@ -5,7 +5,7 @@ from   PIL                     import  Image
 from   io                      import  BytesIO
 
 def cutout(ax, ra, dec):  
-  url       = 'http://legacysurvey.org/viewer/jpeg-cutout?ra={:.4f}&dec={:.4f}&layer=decals-dr7&pixscale=0.27&bands=grz'.format(ra, dec)
+  url       = 'https://www.legacysurvey.org/viewer/cutout.jpg?ra={:.4f}&dec={:.4f}&layer=dr8&pixscale=1.00'.format(ra, dec)
 
   response  = requests.get(url)
   img       = Image.open(BytesIO(response.content))

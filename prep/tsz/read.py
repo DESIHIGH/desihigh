32069943af290c041b78010ad71fb0b3d5e5ed92
michaelJwilson <mjwilson@lbl.gov>
2020-08-20 16:49:00 -0700

    Added Planck sv tsz matches.

---
+++ prep/tsz/read.py
@@ -0,0 +1,47 @@
+import glob
+import pandas          as pd
+import pylab           as pl
+import astropy.io.fits as fits
+import numpy           as np 
+
+from   desimodel.io         import load_tiles
+from   scipy.spatial        import cKDTree as KDTree
+from   desimodel.focalplane import get_tile_radius_deg
+from   astropy.table        import Table
+from   desimodel.footprint  import is_point_in_desi
+
+
+dat   = pd.read_csv('pszmmf1.dat', sep='\s+')
+
+ra    = dat.iloc[:, 3]
+dec   = dat.iloc[:, 4]
+
+tsz   = Table(np.c_[ra, dec], names=['RA', 'DEC'])
+
+## 
+files  = glob.glob('/global/cfs/cdirs/desi/spectro/redux/andes/tiles/*/*/cframe-b0-*.fits')
+plates = []
+
+for p in files:
+    p  = fits.open(p)[0]
+    
+    plates.append([p.header['TILEID'], p.header['TILERA'], p.header['TILEDEC']])
+    
+plates  = np.array(plates)
+
+plates  = Table(plates, names=['TILEID', 'RA', 'DEC'])
+
+
+# tiles = load_tiles(extra=True, onlydesi=False)
+isin, indx = is_point_in_desi(plates, tsz['RA'], tsz['DEC'], return_tile_index=True)
+
+indx   = np.unique(indx).astype(np.int)
+
+plates = plates[indx]
+
+desitsz = tsz[isin]
+
+print(plates)
+
+# andes_tsz = dat[isin]
+# andes_tsz.to_csv('andes_tsz.txt')

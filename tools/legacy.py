663981e7ea64ed6188907255a376774779f83866
Michael Wilson <mjwilson@lbl.gov>
2020-08-28 09:05:05 -0700

    Added timeoute param to legacy cutout query.

--- tools/legacy.py
+++ tools/legacy.py
@@ -1,5 +1,5 @@
 import requests
-import matplotlib.pyplot       as     plt
+import matplotlib.pyplot       as      plt
 
 from   PIL                     import  Image
 from   io                      import  BytesIO
@@ -7,8 +7,15 @@ from   io                      import  BytesIO
 def cutout(ax, ra, dec):  
   url       = 'https://www.legacysurvey.org/viewer/cutout.jpg?ra={:.4f}&dec={:.4f}&layer=dr8&pixscale=1.00'.format(ra, dec)
 
-  response  = requests.get(url)
-  img       = Image.open(BytesIO(response.content))
+  try:
+    # Timeout in seconds.
+    response  = requests.get(url, timeout=10.)
+    img       = Image.open(BytesIO(response.content))
   
-  ax.imshow(img)
+    ax.imshow(img)
+
+  except:
+    print('Failed to retrieve {}'.format(url))
+
+  return  url
   

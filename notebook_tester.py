c03a96baab95ac569daeda6baff3d9defd607a50
Michael Wilson <mjwilson@lbl.gov>
2020-09-10 15:52:20 -0700

    Add tsz to travis papermill checks.

--- notebook_tester.py
+++ notebook_tester.py
@@ -3,24 +3,30 @@ import papermill as pm
 
 pm.execute_notebook(
    './Intro.ipynb',
-   './pmout/pmout.ipynb',
+   './pmout/Intro.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
 pm.execute_notebook(
    './DESI.ipynb',
-   './pmout/pmout.ipynb',
+   './pmout/DESI.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
 pm.execute_notebook(
    './DesigningDESI.ipynb',
-   './pmout/pmout.ipynb',
+   './pmout/DesigningDESI.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
 pm.execute_notebook(
    './SnowWhiteDwarf.ipynb',
-   './pmout/pmout.ipynb',
+   './pmout/SnowWhiteDwarf.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
+   './tsz.ipynb',
+   './pmout/tsz.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )

3004b08af5a652c366c37685719204ae64bc27f5
Michael Wilson <mjwilson@lbl.gov>
2020-09-14 22:40:03 -0700

    cover es notebookes with travis.

--- notebook_tester.py
+++ notebook_tester.py
@@ -8,8 +8,20 @@ pm.execute_notebook(
 )
 
 pm.execute_notebook(
+   './Espanol/Intro_es.ipynb',
+   './pmout/Intro_es.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
    './DESI.ipynb',
    './pmout/DESI.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
+   './Espanol/DESI_es.ipynb',
+   './pmout/DESI_es.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
@@ -20,14 +32,32 @@ pm.execute_notebook(
 )
 
 pm.execute_notebook(
+   './Espanol/DesigningDESI_es.ipynb',
+   './pmout/DesigningDESI_es.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
    './SnowWhiteDwarf.ipynb',
    './pmout/SnowWhiteDwarf.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
 pm.execute_notebook(
+   './Espanol/SnowWhiteDwarf_es.ipynb',
+   './pmout/SnowWhiteDwarf_es.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
    './Clusters.ipynb',
    './pmout/Clusters.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
+   './Espanol/Clusters_es.ipynb',
+   './pmout/Clusters_es.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 

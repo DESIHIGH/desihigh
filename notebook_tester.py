1b700428df48e84d57f66cd356498d8b3af97b1b
Michael Wilson <mjwilson@lbl.gov>
2021-01-27 19:38:08 -0800

    Fix bugs in travis tester.

--- notebook_tester.py
+++ notebook_tester.py
@@ -1,15 +1,10 @@
 import os
 import papermill as pm
 
+
 pm.execute_notebook(
    './Intro.ipynb',
    'desihigh/pmout/Intro.ipynb',
-   parameters=dict(alpha=0.6, ratio=0.1)
-)
-
-pm.execute_notebook(
-   './Espanol/Intro_es.ipynb',
-   'desihigh//pmout/Intro_es.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
@@ -19,21 +14,9 @@ pm.execute_notebook(
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
-#pm.execute_notebook(
-#   './Espanol/DESI_es.ipynb',
-#   'desihigh/pmout/DESI_es.ipynb',
-#   parameters=dict(alpha=0.6, ratio=0.1)
-#)
-
 pm.execute_notebook(
    './DesigningDESI.ipynb',
    'desihigh/pmout/DesigningDESI.ipynb',
-   parameters=dict(alpha=0.6, ratio=0.1)
-)
-
-pm.execute_notebook(
-   './Espanol/DesigningDESI_es.ipynb',
-   'desihigh/pmout/DesigningDESI_es.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
@@ -43,23 +26,11 @@ pm.execute_notebook(
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
-#pm.execute_notebook(
-#   './Espanol/SnowWhiteDwarf_es.ipynb',
-#   'desihigh/pmout/SnowWhiteDwarf_es.ipynb',
-#   parameters=dict(alpha=0.6, ratio=0.1)
-#)
-
 pm.execute_notebook(
    './Clusters.ipynb',
    'desihigh/pmout/Clusters.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
-
-#pm.execute_notebook(
-#   './Espanol/Clusters_es.ipynb',
-#   'desihigh/pmout/Clusters_es.ipynb',
-#   parameters=dict(alpha=0.6, ratio=0.1)
-#)
 
 pm.execute_notebook(
    './FromMayaToDESI.ipynb',
@@ -67,14 +38,52 @@ pm.execute_notebook(
    parameters=dict(alpha=0.6, ratio=0.1)
 )
 
-#pm.execute_notebook(
-#   './Espanol/FromMayaToDESI_es.ipynb',
-#   'desihigh/pmout/FromMayaToDESI_es.ipynb',
-#   parameters=dict(alpha=0.6, ratio=0.1)
-#)
-
 pm.execute_notebook(
    './SupernovaeBrain.ipynb.ipynb',
    'desihigh/pmout/SupernovaeBrain.ipynb',
    parameters=dict(alpha=0.6, ratio=0.1)
 )
+
+os.chdir('./Espanol/')
+
+pm.execute_notebook(
+   './Intro_es.ipynb',
+   '../desihigh//pmout/Intro_es.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
+   './DESI_es.ipynb',
+   '../desihigh/pmout/DESI_es.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
+   './DesigningDESI_es.ipynb',
+   '../desihigh/pmout/DesigningDESI_es.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
+   './SnowWhiteDwarf_es.ipynb',
+   '../desihigh/pmout/SnowWhiteDwarf_es.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(
+   './Clusters_es.ipynb',
+   '../desihigh/pmout/Clusters_es.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+pm.execute_notebook(                                                                                                                                                                                                                      
+   './FromMayaToDESI_es.ipynb',                                                                                                                                                                                                     
+   '../desihigh/pmout/FromMayaToDESI_es.ipynb',                                                                                                                                                                                             
+   parameters=dict(alpha=0.6, ratio=0.1)                                                                                                                                                                                                   
+) 
+
+pm.execute_notebook(                                                                                                                                                                                                                      
+   './SupernovaeBrain_es.ipynb',                                                                                                                                                                                                     
+   '../desihigh/pmout/SupernovaeBrain_es.ipynb',                                                                                                                                                                                              
+   parameters=dict(alpha=0.6, ratio=0.1)
+)            

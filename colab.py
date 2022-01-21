8818c8a8557399b33058f4d93ea3afe1f70a499f
michaelJwilson <michael.j.wilson@durham.ac.uk>
2022-01-21 20:29:35 +0000

    colab v2.

--- colab.py
+++ colab.py
@@ -4,23 +4,29 @@ import subprocess
 from   google.colab import drive
 
 
-def init_colab():
-    drive='/content/drive/MyDrive/'
+drive='/content/drive/'
 
+if 'COLAB_GPU' in os.environ:
+    # 'Hmmm, seems you\'re not in colab :)  Try again later.'
     drive.mount(drive, force_remount=True)
 
-    drive = drive + '/MyDrive/'
+    mydrive = drive + '/MyDrive/'
+    
+    os.chdir(mydrive)
+    
+    try:
+        import desihigh
+        
+    except:
+        print('Failed to import desihigh; Cloning.')
 
-    os.chdir(drive)
+        subprocess.run('git clone https://github.com/michaelJwilson/desihigh.git', shell=True, check=True)    
+        # subprocess.run('pip install -r desihigh/requirements.txt', shell=True, check=True)
 
-    subprocess.run('git clone https://github.com/michaelJwilson/desihigh.git', shell=True, check=True)    
-    subprocess.run('pip install -r desihigh/requirements.txt', shell=True, check=True)
-
-    sys.path.append(drive + '/desihigh/')
+    sys.path.append(mydrive + '/desihigh/')
 
 def save_colab():
-    drive='/content/drive/MyDrive/'
+    if 'COLAB_GPU' in os.environ:
+        # drive.mount(drive, force_remount=True)
 
-    drive.mount(drive, force_remount=True)
-
-    drive.flush_and_unmount()
+        drive.flush_and_unmount()

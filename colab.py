de4d6f34041255aecac1ac2a8bac560ece1a97d9
Alma <alma.gonzalez@fisica.ugto.mx>
2022-01-25 22:53:22 -0600

    Update colab.py

--- colab.py
+++ colab.py
@@ -13,14 +13,14 @@ if 'COLAB_GPU' in os.environ:
 
     mydrive = drivepath + '/MyDrive/'
     
-    os.chdir(mydrive)
+    sys.path.append(mydrive + '/desihigh/')
     
     try:
         import  desihigh
         
     except:
         print('Failed to import desihigh; Cloning.')
-
+        os.chdir(mydrive)
         subprocess.run('git clone https://github.com/michaelJwilson/desihigh.git', shell=True, check=True)    
         # subprocess.run('pip install -r desihigh/requirements.txt', shell=True, check=True)
 
@@ -32,7 +32,7 @@ if 'COLAB_GPU' in os.environ:
 
             raise RuntimeError(emessage)
 
-    sys.path.append(mydrive + '/desihigh/')
+    os.chdir(mydrive+'/desihigh')
 
 def save_colab():
     if 'COLAB_GPU' in os.environ:
